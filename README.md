4e449f906063fbbc4145967169915979119767d8
yinyong02 <yinyong02@baidu.com>
2016-06-16 17:00:54 +0800

    worked

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # antiaris-filestamp
-File stamp for antiaris
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency status][david-dm-image]][david-dm-url] [![Dev Dependency status][david-dm-dev-image]][david-dm-dev-url]
+
+文件时间戳
+
+```js
+import {filestamp, contentstamp} from 'antiaris-filestamp';
+
+filestamp('./img/logo.png', (err, result) => {
+    result//{filename: logo_e65c0a.png}
+});
+
+filestamp.sync('./img/logo.png');
+
+contentstamp('var a=1;', 'log0.png', (err, result) => {
+    result//{filename: logo_e65c0a.png}
+});
+
+contentstamp.sync('var a=1;', 'log0.png');
+
+```
+
+[npm-url]: https://npmjs.org/package/antiaris-filestamp
+[downloads-image]: http://img.shields.io/npm/dm/antiaris-filestamp.svg
+[npm-image]: http://img.shields.io/npm/v/antiaris-filestamp.svg
+[travis-url]: https://travis-ci.org/antiaris/antiaris-filestamp
+[travis-image]: http://img.shields.io/travis/antiaris/antiaris-filestamp.svg
+[david-dm-url]:https://david-dm.org/antiaris/antiaris-filestamp
+[david-dm-image]:https://david-dm.org/antiaris/antiaris-filestamp.svg
+[david-dm-dev-url]:https://david-dm.org/antiaris/antiaris-filestamp#info=devDependencies
+[david-dm-dev-image]:https://david-dm.org/antiaris/antiaris-filestamp/dev-status.svg
---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2016 yanni4night.com
+ * index.js
+ *
+ * changelog
+ * 2016-06-16[16:08:31]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+const Numbase = require('numbase');
+
+const base = new Numbase();
+
+const getDigest = content => {
+    let digest = crypto.createHash('md5').update(content).digest('hex');
+    digest = (parseInt(digest, 16) % 1e10).toString(16);
+    return base.encode(base.decode(digest, 16));
+}
+
+const rename = (filename, stamp) => {
+    const ext = path.extname(filename);
+    return path.basename(filename, ext) + `_${stamp}${ext}`;
+};
+
+const filestamp = exports.filestamp = (filename, cb) => {
+    fs.readFile(filename, 'utf-8', (err, content) => {
+        if (err) {
+            cb(err);
+        } else {
+            const stamp = getDigest(content);
+            cb(null, {
+                filename: rename(filename, stamp)
+            });
+        }
+    });
+};
+
+filestamp.sync = filename => {
+    const content = fs.readFileSync(filename, 'utf-8');
+    const stamp = getDigest(content);
+    return {
+        filename: rename(filename, stamp)
+    };
+};
+
+const contentstamp = exports.contentstamp = (content, filename, cb) => {
+    const stamp = getDigest(content);
+    cb(null, {
+        filename: rename(filename, stamp)
+    });
+};
+
+contentstamp.sync = (content, filename) => {
+    const stamp = getDigest(content);
+    return {
+        filename: rename(filename, stamp)
+    };
+};
